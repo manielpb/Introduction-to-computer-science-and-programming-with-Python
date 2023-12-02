3951634f182e87f5937bbc93816deee016b36e8b
maniel <102647979+manielpb@users.noreply.github.com>
2023-12-02 15:28:47 +0000

    Update ps0.py

--- ps0.py
+++ ps0.py
@@ -1,6 +1,6 @@
 import math
 
-x = int(input("Enter number x: "))
+# A Very Simple Program: Raising a number to a power and taking a logarithmx = int(input("Enter number x: "))
 y = int(input("Enter number y: "))
 z= x ** y
 v = int(math.log2(x))
